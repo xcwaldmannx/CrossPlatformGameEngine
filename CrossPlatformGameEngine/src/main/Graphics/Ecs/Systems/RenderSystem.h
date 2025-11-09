f4cdc293a958dbc068682f99ed5a229dbab0ddae
Christian Waldmann <xcwaldmannx@gmail.com>
2025-11-08 21:32:18 -0500

    fixed per-mesh transforms, added helicopter example

--- CrossPlatformGameEngine/src/main/Graphics/Ecs/Systems/RenderSystem.h
+++ CrossPlatformGameEngine/src/main/Graphics/Ecs/Systems/RenderSystem.h
@@ -31,6 +31,22 @@ struct ModelData
 	std::vector<uint32_t> mTransformOffsets;
 };
 
+struct MeshKey {
+	uint32_t modelId;
+	uint32_t meshIndex;
+
+	bool operator==(const MeshKey& other) const noexcept {
+		return modelId == other.modelId && meshIndex == other.meshIndex;
+	}
+};
+
+// For unordered_map:
+struct MeshKeyHasher {
+	size_t operator()(const MeshKey& k) const noexcept {
+		return (static_cast<size_t>(k.modelId) << 32) ^ k.meshIndex;
+	}
+};
+
 class RenderSystem : public System_I
 {
 public:
@@ -39,57 +55,63 @@ public:
 
 	void update(float deltaTime) override
 	{
-		mModelGroups.clear();
+		mMeshGroups.clear();
 		mInstances.clear();
 		mDrawCommands.clear();
 
 		for (EntityId e : mEntities)
 		{
 			auto& model = mComponentManager->getComponent<ModelComponent>(e);
-
 			if (model.mIsHidden) continue;
 
+			ModelData& modelData = mModelData->at(model.mModelId);
 			auto& transform = mComponentManager->getComponent<TransformComponent>(e);
 
-			glm::quat rotQuat = glm::quat(transform.mRotation);
-			glm::mat4 rotMatrix = glm::toMat4(rotQuat);
+			glm::mat4 T0 = glm::translate(glm::mat4(1.0f), transform.mPosition);
+			glm::mat4 R0 = glm::toMat4(glm::quat(transform.mRotation));
+			glm::mat4 S0 = glm::scale(glm::mat4(1.0f), transform.mScale);
 
-			ModelData& modelData = mModelData->at(model.mModelId);
+			glm::mat4 modelMatrix = T0 * R0 * S0;
 
 			for (uint32_t i = 0; i < modelData.mMeshCount; i++)
 			{
 				GPUInstance instance{};
-				instance.mTransform =
-					(glm::translate(glm::mat4(1.0f), transform.mPosition)
-						* rotMatrix
-						* glm::scale(glm::mat4(1.0f), transform.mScale));
 
-				mModelGroups[model.mModelId].emplace_back(std::move(instance));
+				const auto& meshTransform = model.mMeshTransforms[i];
+
+				glm::mat4 T1 = glm::translate(glm::mat4(1.0f), meshTransform.mPosition);
+				glm::mat4 R1 = glm::toMat4(glm::quat(meshTransform.mRotation));
+				glm::mat4 S1 = glm::scale(glm::mat4(1.0f), meshTransform.mScale);
+
+				glm::mat4 meshMatrix = T1 * R1 * S1;
+
+				instance.mTransform = modelMatrix * meshMatrix;
+
+				MeshKey key{ model.mModelId, i };
+				mMeshGroups[key].emplace_back(std::move(instance));
 			}
-
 		}
 
+
 		uint32_t firstInstance = 0;
-
-		for (auto& [modelId, instances] : mModelGroups)
+		for (auto& [key, instances] : mMeshGroups)
 		{
-			ModelData& modelData = mModelData->at(modelId);
+			const ModelData& modelData = mModelData->at(key.modelId);
+			uint32_t meshIndex = key.meshIndex;
 
-			for (uint32_t i = 0; i < modelData.mMeshCount; i++)
-			{
-				VkDrawIndexedIndirectCommand cmd{};
-				cmd.vertexOffset  = static_cast<int32_t>(modelData.mVertexOffsets[i]);
-				cmd.firstIndex    = modelData.mIndexOffsets[i];
-				cmd.indexCount    = modelData.mIndexCounts[i];
-				cmd.firstInstance = firstInstance;
-				cmd.instanceCount = static_cast<uint32_t>(instances.size());
-				mDrawCommands.emplace_back(std::move(cmd));
-			}
+			VkDrawIndexedIndirectCommand cmd{};
+			cmd.vertexOffset = static_cast<int32_t>(modelData.mVertexOffsets[meshIndex]);
+			cmd.firstIndex = modelData.mIndexOffsets[meshIndex];
+			cmd.indexCount = modelData.mIndexCounts[meshIndex];
+			cmd.firstInstance = firstInstance;
+			cmd.instanceCount = static_cast<uint32_t>(instances.size());
 
+			mDrawCommands.emplace_back(cmd);
 			mInstances.insert(mInstances.end(), instances.begin(), instances.end());
 
 			firstInstance += static_cast<uint32_t>(instances.size());
 		}
+
 	}
 
 	const std::vector<GPUInstance>& getInstances() const
@@ -105,7 +127,7 @@ public:
 private:
 	std::unordered_map<uint32_t, ModelData>* mModelData = nullptr;
 
-	std::unordered_map<uint32_t, std::vector<GPUInstance>> mModelGroups; // mesh ID -> instances
+	std::unordered_map<MeshKey, std::vector<GPUInstance>, MeshKeyHasher> mMeshGroups; // mesh ID -> instances
 	std::vector<GPUInstance> mInstances;
 	std::vector<VkDrawIndexedIndirectCommand> mDrawCommands;
 
